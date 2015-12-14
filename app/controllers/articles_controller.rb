34739c6ae6d5f384fc94d9bbe1716805be096575
shun-y <vis.nt.rstd@gmail.com>
2015-12-14 11:15:14 +0900

    トップページのデザインを変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
     def index 
       @articles = Article.paginate(page: params[:page])
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'articles#index'
   get 'users/new'
   resources :users
+  resources :articles
+
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
